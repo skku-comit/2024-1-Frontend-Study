8fbe85fdb436d6b271f74febb1d1f47cb5726841
YumeMana <manamana32321@gmail.com>
2024-05-30 18:45:54 +0900

    chore: merge previous code

--- manamana32321/12w/src/App.tsx
+++ manamana32321/12w/src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import StudyList from './components/StudyList';
+import StudyDetail from './components/StudyDetail';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <a href='/' className="text-center p-3 pt-4 fw-bold">
+            <h2>CoMit Study</h2>
+          </a>
+        </header>
+
+        <main id="content">
+          <Switch>
+            <Route exact path="/">
+              <StudyList />
+            </Route>
+            
+            <Route path="/study/:studyId">
+              <StudyDetail/>
+            </Route>
+
+            <Route>
+              <p>Error: 404</p>
+            </Route>
+          </Switch>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ manamana32321/12w/src/components/StudyCard.tsx
@@ -0,0 +1,54 @@
+import React from "react"
+import { Study } from "../data"
+import { Card, CardBody, CardTitle, CardText } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import Badge from "./Badge"
+
+interface StudyCardProps {
+  id: number
+  study: Study
+}
+
+const StudyCard: React.FC<StudyCardProps> = ({ id, study }) => {
+  const { imageSrc, title, level, stack, campus } = study
+
+  return (
+    <Link to={`/study/${id}`}>
+      <Card className='text-center p-3'>
+        <CardBody>
+          <img alt={title} src={imageSrc} className='mb-3' width='160' />
+          <CardTitle className='fw-bold mb-3'>{title}</CardTitle>
+
+          <div className='mb-3'>
+            {[level, stack[0], campus].map((text) => {
+              return <Badge key={text} text={text} />
+            })}
+          </div>
+        </CardBody>
+      </Card>
+    </Link>
+  )
+}
+
+export const StudyLoadingCard = () => {
+  return(
+    <Card className='text-center p-3'>
+      <CardBody>
+        <img alt='...' src='...' className='card-img-top mb-3' width='160' />
+        <CardTitle className='fw-bold mb-3 placeholder-glow'>
+          <span className="placeholder col-12"></span>
+        </CardTitle>
+
+        <CardText className="card-text placeholder-glow">
+          <span className="placeholder col-4"></span>
+          <span className="placeholder col-2"></span>
+          <span className="placeholder col-2"></span>
+          <span className="placeholder col-3"></span>
+          <span className="placeholder col-3"></span>
+        </CardText>
+      </CardBody>
+    </Card>
+  )
+}
+
+export default StudyCard
